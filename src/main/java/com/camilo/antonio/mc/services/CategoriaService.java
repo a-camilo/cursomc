682d1249b096b9e68c7fa972e0010a0c7c243cb1
Antonio Camilo <acamilo10@hotmail.com>
2022-09-28 20:05:38 -0300

    Criando repository e service para categoria

---
+++ src/main/java/com/camilo/antonio/mc/services/CategoriaService.java
@@ -0,0 +1,21 @@
+package com.camilo.antonio.mc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.camilo.antonio.mc.domain.Categoria;
+import com.camilo.antonio.mc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+	
+	@Autowired
+	private CategoriaRepository repo;
+	
+	public Categoria buscar(Integer id) {
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElse(null);
+	}
+}
